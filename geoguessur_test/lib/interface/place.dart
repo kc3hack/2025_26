59d696b4e2556fb5b195a6f6c1e408be02c04d14
kaja36 <kamowaka4m7@gmail.com>
2025-02-18 21:57:38 +0900

    型追加

---
+++ geoguessur_test/lib/interface/place.dart
@@ -0,0 +1,25 @@
+class Place {
+  Place(
+      {required this.id,
+      required this.name,
+      required this.address,
+      required this.category,
+      required this.year});
+  /*
+    place
+    id: num
+    name:str
+    adress:srt
+    category:str
+    year:date
+    popularity:
+    url: str 
+  */
+
+  //途中
+  int id;
+  String name;
+  String address;
+  String category;
+  String year;
+}
