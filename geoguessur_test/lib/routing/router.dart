d262e28cc065cb789a57e48c550f4df5961736e5
Ura <mikeneko41@icloud.com>
2025-02-18 14:44:23 +0900

    flutter upgrade

---
+++ geoguessur_test/lib/routing/router.dart
@@ -0,0 +1,24 @@
+import 'package:geoguessur_test/screens/about_screen.dart';
+import 'package:geoguessur_test/screens/home_screen.dart';
+import 'package:geoguessur_test/screens/setting_screen.dart';
+import 'package:go_router/go_router.dart';
+
+
+final router = GoRouter(
+  debugLogDiagnostics: true,
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+        name: 'home',
+        path: '/',
+        builder: (context, state) => const HomeScreen()),
+    GoRoute(
+        name: 'about',
+        path: '/about',
+        builder: (context, state) => const AboutScreen()),
+    GoRoute(
+        name: 'setting',
+        path: '/setting',
+        builder: (context, state) => const SettingScreen()),
+  ],
+);
---
+++ geoguessur_test/lib/screens/about_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('About'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            context.go('/setting');
+          },
+          child: const Text(
+            'Go To Setting Screen',
+          ),
+        ),
+      ),
+    );
+  }
+}
