d8bd4bd2279f18fa4569fe19793f5b9dfb3c5605
Ura <mikeneko41@icloud.com>
2025-02-19 17:04:55 +0900

    ロジック自体完成

--- geoguessur_test/lib/screens/guessr/quiz_screen.dart
+++ geoguessur_test/lib/screens/guessr/quiz_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:geoguessur_test/utils/get_location.dart';
-import 'package:geoguessur_test/utils/translate_location.dart';
+import 'package:geoguessur_test/utils/location_info/get_location_info.dart';
+import 'package:geoguessur_test/utils/location_info/translate_location.dart';
 import 'package:go_router/go_router.dart';
 
 class QuizScreen extends StatelessWidget {
   const QuizScreen({super.key, required this.level});
   final int level;
-
+  final testAdressString = '大阪府大阪市住吉区住吉２丁目９−８９';
+  final double maxDistance = 50000; // 大阪府の端から端までの長さの半分（メートル）
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,8 +17,20 @@ class QuizScreen extends StatelessWidget {
           onPressed: () async {
             try {
               final location = await getCurrentPosition();
-              final adress = await transLateLocateToAddress(location);
-              context.go('./result', extra: adress);
+              final quizeLocation = await transLateAddressToLocate(
+                testAdressString,
+              );
+              final distance = await getDistanceBetweenTwoPoints(
+                location.latitude,
+                location.longitude,
+                quizeLocation.latitude,
+                quizeLocation.longitude,
+              );
+              final score =
+                  ((maxDistance - distance) / maxDistance * 100)
+                      .clamp(0, 100)
+                      .toInt();
+              context.go('./result', extra: score);
             } catch (e) {
               // エラー処理
               ScaffoldMessenger.of(
