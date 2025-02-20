e907c7b869d469a0eeb96f2fc6a77b6e0bc12aab
Ura <mikeneko41@icloud.com>
2025-02-20 18:27:58 +0900

    フォント以外最初の画面完了

--- geoguessur_test/lib/screens/guessr/guessr_screen.dart
+++ geoguessur_test/lib/screens/guessr/guessr_screen.dart
@@ -3,24 +3,51 @@ import 'package:go_router/go_router.dart';
 
 class GuessrScreen extends StatelessWidget {
   const GuessrScreen({super.key});
+  final imgLPosition = -0.3;
+  final buttonMargin = 150.0;
+  final Map<String, double> buttonSize = const {
+    "horizontal": 50.0,
+    "vertical": 20.0,
+  };
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('guessr')),
-      body: new Stack(
+      body: Stack(
         children: <Widget>[
-          new Image.asset("images/guessur_thumbnail.JPG", fit: BoxFit.cover),
-          new Center(
-            child: ElevatedButton(
-              onPressed: () {
-                context.go('/guessr/level');
-              },
-              child: const Text('Go To level'),
+          Positioned.fill(
+            child: Image.asset(
+              "assets/images/guessur_thumbnail.JPG",
+              fit: BoxFit.cover,
+              alignment: Alignment(imgLPosition, 0),
+            ),
+          ),
+          Align(
+            alignment: new Alignment(0.0, 1.0),
+            child: Container(
+              margin: new EdgeInsets.only(bottom: buttonMargin),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10), // 四角にする
+                  ),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: buttonSize["horizontal"]!,
+                    vertical: buttonSize["vertical"]!,
+                  ), // サイズを大きくする
+                ),
+                onPressed: () {
+                  context.go('/guessr/level');
+                },
+                child: const Text(
+                  '開始',
+                  style: TextStyle(fontSize: 20, fontFamily: 'Tamanegi'),
+                ),
+              ),
             ),
           ),
         ],
-        fit: StackFit.expand,
       ),
     );
   }
