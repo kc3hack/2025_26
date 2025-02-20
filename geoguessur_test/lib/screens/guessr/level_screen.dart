ebc22c0fdf9ba555a9a59e56b07f04d9ad034e45
Ura <mikeneko41@icloud.com>
2025-02-20 18:41:45 +0900

    画面2をボタンで仮置き

--- geoguessur_test/lib/screens/guessr/level_screen.dart
+++ geoguessur_test/lib/screens/guessr/level_screen.dart
@@ -3,40 +3,64 @@ import 'package:go_router/go_router.dart';
 
 class LevelScreen extends StatelessWidget {
   const LevelScreen({super.key});
+  final imgLPosition = -0.3;
+  final buttonMargin = 150.0;
+  final Map<String, double> buttonSize = const {
+    "horizontal": 50.0,
+    "vertical": 20.0,
+  };
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('level')),
-      body: Center(
-        child: Column(
-          children: [
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  context.go('/guessr/level/1/quiz');
-                },
-                child: const Text('Go To quiz level 1'),
+      body: Stack(
+        children: <Widget>[
+          Positioned.fill(
+            child: Opacity(
+              opacity: 0.5,
+              child: Image.asset(
+                "assets/images/guessur_thumbnail.JPG",
+                fit: BoxFit.cover,
+                alignment: Alignment(imgLPosition, 0),
               ),
             ),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  context.go('/guessr/level/2/quiz');
-                },
-                child: const Text('Go To quiz level 2'),
-              ),
+          ),
+          Align(
+            alignment: new Alignment(0.0, 0.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center, // 中央に配置
+              children: [
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      context.go('/guessr/level/1/quiz');
+                    },
+                    child: const Text('Go To quiz level 1'),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      context.go('/guessr/level/2/quiz');
+                    },
+                    child: const Text('Go To quiz level 2'),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      context.go('/guessr/level/3/quiz');
+                    },
+                    child: const Text('Go To quiz level 3'),
+                  ),
+                ),
+              ],
             ),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  context.go('/guessr/level/3/quiz');
-                },
-                child: const Text('Go To quiz level 3'),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
