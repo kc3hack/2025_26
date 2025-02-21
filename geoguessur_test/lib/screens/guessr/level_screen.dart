52225098cafeb365f6d2fbdfbaf28b442cef263b
Ura <mikeneko41@icloud.com>
2025-02-21 21:31:37 +0900

    レベルの見た目変更

--- geoguessur_test/lib/screens/guessr/level_screen.dart
+++ geoguessur_test/lib/screens/guessr/level_screen.dart
@@ -27,34 +27,100 @@ class LevelScreen extends StatelessWidget {
             ),
           ),
           Align(
-            alignment: new Alignment(0.0, 0.0),
+            alignment: Alignment(0.0, 0.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center, // 中央に配置
               children: [
                 Center(
-                  child: ElevatedButton(
-                    onPressed: () {
+                  child: ListTile(
+                    leading: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      color: Colors.blue,
+                      child: Text(
+                        '簡単',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                      'Level 1: 初級',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      '簡単なレベルのクイズです。',
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                    onTap: () {
                       context.go('/guessr/level/1/quiz');
                     },
-                    child: const Text('Go To quiz level 1'),
                   ),
                 ),
                 const SizedBox(height: 20),
                 Center(
-                  child: ElevatedButton(
-                    onPressed: () {
+                  child: ListTile(
+                    leading: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      color: Colors.green,
+                      child: Text(
+                        '府県内',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                      'Level 2: 中級',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      '中級レベルのクイズです。',
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                    onTap: () {
                       context.go('/guessr/level/2/quiz');
                     },
-                    child: const Text('Go To quiz level 2'),
                   ),
                 ),
                 const SizedBox(height: 20),
                 Center(
-                  child: ElevatedButton(
-                    onPressed: () {
+                  child: ListTile(
+                    leading: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      color: Colors.red,
+                      child: Text(
+                        '近畿',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                      'Level 3: 上級',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      '難しいレベルのクイズです。',
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                    onTap: () {
                       context.go('/guessr/level/3/quiz');
                     },
-                    child: const Text('Go To quiz level 3'),
                   ),
                 ),
               ],
