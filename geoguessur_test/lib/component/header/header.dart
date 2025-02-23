bc98572ae07ac9c63e3567b0be3baa5708675741
Hiroto Kusakabe <2312110352x@kindai.ac.jp>
2025-02-23 14:21:42 +0900

    ヘッダーの実装

---
+++ geoguessur_test/lib/component/header/header.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class Header extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      bottom: TabBar(
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              context.go('/');
+              break;
+            case 1:
+              context.go('/list');
+              break;
+            case 2:
+              context.go('/guessr');
+              break;
+          }
+        },
+        tabs: [Tab(text: "Main"), Tab(text: "List"), Tab(text: "Guessr")],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight + 0); // AppBar + TabBar の高さ
+}
