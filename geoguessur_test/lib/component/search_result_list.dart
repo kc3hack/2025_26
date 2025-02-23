eb5f24e6be9bc13847622380e0a45e10e553a1de
kaja36 <kamowaka4m7@gmail.com>
2025-02-23 15:13:13 +0900

    詳細画面作成

--- geoguessur_test/lib/component/search_result_list.dart
+++ geoguessur_test/lib/component/search_result_list.dart
@@ -1,6 +1,7 @@
 //リスト作成ウィジェット
 import 'package:flutter/material.dart';
 import 'package:geoguessur_test/interface/place.dart';
+import 'package:go_router/go_router.dart';
 
 class SearchResultList extends StatelessWidget {
   const SearchResultList({super.key, required this.resultPlaces});
@@ -18,7 +19,7 @@ class SearchResultList extends StatelessWidget {
             GestureDetector(
               onTap: () {
                 print(resultPlaces.elementAt(index).name);
-                //context.push(''); 詳細情報へ
+                context.push('/detail', extra: resultPlaces.elementAt(index));
               },
               child: Container(
                 color: Colors.transparent,
--- geoguessur_test/lib/screens/home/detail_screen.dart
+++ geoguessur_test/lib/screens/home/detail_screen.dart
@@ -1,13 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:geoguessur_test/interface/place.dart';
 
 class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
+  const DetailScreen({super.key, required this.place});
+  final Place place;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Detail")),
-      body: Center(child: Text("Hello .")),
+      body:  Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+        Image.network(place.imageUrl),
+        Padding(
+          padding: const EdgeInsets.all(12),
+          child: Text(place.name,style: TextStyle(fontSize: 22),),
+        ),
+        Divider(),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('住所:',style: TextStyle(fontSize: 16),),
+              Text('  〇〇県〇〇市AA区BB町CC', style: TextStyle(fontSize: 15),),
+            ],
+          ),
+        ),
+        Divider(),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('アクセス:',style: TextStyle(fontSize: 16),),
+              Text('  〇〇県〇〇市AA区BB町CC', style: TextStyle(fontSize: 15),),
+            ],
+          ),
+        ),
+        Divider(),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('説明:',style: TextStyle(fontSize: 16),),
+              Text('  〇〇県〇〇市AA区BB町CC', style: TextStyle(fontSize: 15),),
+            ],
+          ),
+        ),
+        Divider(),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('豆知識:',style: TextStyle(fontSize: 16),),
+              Text('  〇〇県〇〇市AA区BB町CC', style: TextStyle(fontSize: 15),),
+            ],
+          ),
+        ),
+        Divider(),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('公式ホームページ:',style: TextStyle(fontSize: 16),),
+              Text('  〇〇県〇〇市AA区BB町CC', style: TextStyle(fontSize: 15),),
+            ],
+          ),
+        ),
+      ],)
     );
   }
 }
