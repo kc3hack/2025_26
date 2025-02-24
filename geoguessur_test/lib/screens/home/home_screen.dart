a72e5ef9c8267a4032410991592d4e6e015b48f2
northemir <130894060+northemir@users.noreply.github.com>
2025-02-24 10:09:36 +0900

    Revert "Feature/やること#31"

--- geoguessur_test/lib/screens/home/home_screen.dart
+++ geoguessur_test/lib/screens/home/home_screen.dart
@@ -1,16 +1,9 @@
 import 'dart:convert';
-import 'dart:ffi';
 import 'package:flutter/material.dart';
-import 'package:geoguessur_test/interface/place.dart';
-import 'package:geoguessur_test/screens/home/detail_screen.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:geoguessur_test/env.dart';
 import 'package:geoguessur_test/component/header/header.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:geoguessur_test/service/database/firestore_service.dart';
-import 'package:go_router/go_router.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -21,60 +14,46 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late GoogleMapController mapController;
-  List<Place> places = [];
 
   final LatLng _center = const LatLng(34.881563, 135.347433);
 
   @override
   void initState() {
     super.initState();
-    _fetchPlaces(places);
+    _fetchPlaces();
   }
 
   Set<Marker> _markers = {};
-
-  Future<void> _fetchPlaces(List<Place> places) async {
+  Future<void> _fetchPlaces() async {
     final String apiKey = Env.pass1; // 取得したAPIキー
-    // const List<String> keywords = [];
+    final String url =
+        "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+        "?location=34.881563,135.347433"
+        "&radius=50000"
+        "&type=place_of_worship"
+        "&keyword=寺 OR 神社"
+        "&key=$apiKey";
 
-    FirestoreService firestoreService = FirestoreService();
-    places = await firestoreService.getAllPlaces();
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final List places = data['results'];
 
-    for (var place in places) {
-      var tmp = place.address;
-      var url =
-          "https://maps.googleapis.com/maps/api/place/textsearch/json"
-          "?query=${Uri.encodeComponent(tmp)}"
-          "&key=$apiKey";
-
-      print(url);
-
-      final response = await http.get(Uri.parse(url));
-      if (response.statusCode == 200) {
-        final data = json.decode(response.body);
-        final List places2 = data['results'];
-
-        setState(() {
-          _markers.addAll(
-            places2.map((place2) {
-              final lat = place2['geometry']['location']['lat'];
-              final lng = place2['geometry']['location']['lng'];
+      setState(() {
+        _markers =
+            places.map((place) {
+              final lat = place['geometry']['location']['lat'];
+              final lng = place['geometry']['location']['lng'];
               return Marker(
-                markerId: MarkerId(place.id.toString()),
+                markerId: MarkerId(place['name']),
                 position: LatLng(lat, lng),
-                infoWindow: InfoWindow(
-                  title: place.name,
-                  snippet: 'ここをタップして詳細情報を見る',
-                  onTap: () => context.go('/detail', extra: place),
-                ),
+                infoWindow: InfoWindow(title: place['name']),
               );
-            }).toSet(),
-          );
-        });
-      }
-      print("Response status: ${response.statusCode}");
-      print("Response body: ${response.body}");
+            }).toSet();
+      });
     }
+    print("Response status: ${response.statusCode}");
+    print("Response body: ${response.body}");
   }
 
   void _onMapCreated(GoogleMapController controller) {
@@ -98,3 +77,4 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
